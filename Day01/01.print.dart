a937174c0c60592671da3485486b210a83631d0c
Jakaria Saikat Dhrobo <113936334+saikatdhrobo@users.noreply.github.com>
2025-10-30 10:14:50 +0600

    Initial commit

---
+++ Day01/01.print.dart
@@ -0,0 +1,16 @@
+/*Today
+I'll learn how to print & comment writing in Dart
+*/
+
+void main()
+{
+  print('Saikat');
+  print("Hello Bangladesh");
+  print(3);
+
+  //Dart follows BODMAS
+  print((3+2)*(4/2));
+
+  print('3'+'2'); //two string concat 
+  print('3'*2); //one string multiply into two times
+}
