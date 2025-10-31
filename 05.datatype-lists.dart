a398498a49bd29e60711b97d3d3cf96340771fe5
Jakaria Saikat Dhrobo <113936334+saikatdhrobo@users.noreply.github.com>
2025-11-01 00:44:08 +0600

    Auto commit on Sat 11/01/2025  0:44:08.38

---
+++ 05.datatype-lists.dart
@@ -0,0 +1,10 @@
+void main(){
+  var city = ['Dhaka','Chattogram','Rajshahi','Barishal'];
+  print(city); //show all names inside the list
+  print(city[1]); //show particular cityName from list. Index no start at 0. If 0 here, 'Dhaka'
+
+  var myList = ['Dhaka','Chattogram', 10, 12.22 , true]; //we can put any variables into the list
+  print(myList);
+  print(myList[4]);
+
+}
