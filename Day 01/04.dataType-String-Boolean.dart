57fb98374e07828e2c59d2da9a51474f92b60e1d
Jakaria Saikat Dhrobo <113936334+saikatdhrobo@users.noreply.github.com>
2025-11-01 00:34:28 +0600

    Auto commit on Sat 11/01/2025  0:34:28.11

---
+++ Day 01/04.dataType-String-Boolean.dart
@@ -0,0 +1,12 @@
+void main(){
+  //string
+  var myself = 'Saikat';
+  var myCountry = "My country is Bangladesh";
+  print(myself);
+  print(myCountry);
+
+  //boolean = yes/no
+  var isBangladeshi = true;
+  print(isBangladeshi);
+
+}
