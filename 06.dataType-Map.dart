c45967a1dad7fc2218bb1bcce0110ba717326f75
Jakaria Saikat Dhrobo <113936334+saikatdhrobo@users.noreply.github.com>
2025-11-01 00:56:40 +0600

    Auto commit on Sat 11/01/2025  0:56:40.22

---
+++ 06.dataType-Map.dart
@@ -0,0 +1,15 @@
+//Two option in map. One is key/property and other is Value and all of them are stored inside Curly braces
+// Key and value can be any type . Key must be unique
+
+void main(){
+ var person = {
+  'name': 'John',
+  'age' : 40,
+  'city' : 'London',
+  'height' : '140cm'
+  };
+
+  print(person);
+  print(person['city']);
+
+}
