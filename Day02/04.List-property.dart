e6c38bb7ba2721423ea23816fab33c9620cdce48
Jakaria Saikat Dhrobo <113936334+saikatdhrobo@users.noreply.github.com>
2025-11-01 22:18:47 +0600

    Auto commit on Sat 11/01/2025 22:18:47.85

---
+++ Day02/04.List-property.dart
@@ -0,0 +1,14 @@
+void main(){
+  var city = ['Dhaka','Rangpur', 'Rajshahi', 'Bagura'];
+  //After writing List variable name, give . (dot) to see properties of list
+    var length= city.length;
+    var reversed= city.reversed;
+    var first =  city.first;
+  city.isEmpty;
+  city.isNotEmpty;
+  city.last;
+
+  print(length);
+  print(reversed);
+  print(first);
+}
