9d1547c6314bdc67ca65853bcceb8e8d0480499a
Jakaria Saikat Dhrobo <113936334+saikatdhrobo@users.noreply.github.com>
2025-11-01 22:39:02 +0600

    Auto commit on Sat 11/01/2025 22:39:01.95

--- Day02/06.List-insert.dart
+++ Day02/06.List-insert.dart
@@ -1,13 +1,13 @@
 void main(){
   var numbers = [1,2,3,4,5,6];
-  numbers.add(7);
+  numbers.add(7); // to add just one element
   print(numbers);
-  numbers.addAll([8,9,10]);
-  print(numbers);
-
-  numbers.insert(3, 100);
+  numbers.addAll([8,9,10]); // to add multiple elements
   print(numbers);
 
-  numbers.insertAll(4, [101,102,103,104]);
+  numbers.insert(3, 100); // to add one element in an index
+  print(numbers);
+
+  numbers.insertAll(4, [101,102,103,104]); // to add multiple elements in an index
   print(numbers);
 }
