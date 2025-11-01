3647ce54340a07812c066303128d1d0bfb7e71c5
Jakaria Saikat Dhrobo <113936334+saikatdhrobo@users.noreply.github.com>
2025-11-01 22:12:26 +0600

    Auto commit on Sat 11/01/2025 22:12:26.61

---
+++ Day02/03.constant.dart
@@ -0,0 +1,15 @@
+/* define as 'final' or 'const' keyword.
+  
+  */
+
+  void main(){
+    const thisYear = 2025;
+    print(thisYear);
+    // thisYear = 2030;
+    // print(thisYear); //Can't re-assign to the const variable as 2025 is a constant defined here
+  }
+
+  /* 
+  final = fixed later (runtime) such as final time = DateTime.now();
+  const = fixed always (compile time) such as const pi = 3.14;
+ */
