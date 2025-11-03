1ab146617d95098d2e81e7ed36e7276b1a168fd7
Jakaria Saikat Dhrobo <113936334+saikatdhrobo@users.noreply.github.com>
2025-11-03 22:36:37 +0600

    Auto commit on Mon 11/03/2025 22:36:37.82

---
+++ Day04/03.switch-case.dart
@@ -0,0 +1,22 @@
+// Conditional Statement 'switch'
+
+void main(){
+  var marks = 81;
+
+  switch(marks){
+    case 80:
+    print("a+");
+    break;
+
+    case 70:
+    print("A-");
+    break;
+    case 60:
+    print("A-");
+    break;
+
+    default:
+    print("Result not found");
+    break;
+  }
+}
