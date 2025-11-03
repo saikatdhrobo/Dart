7724799496eb35c88185bf613b0934c1cc42a360
Jakaria Saikat Dhrobo <113936334+saikatdhrobo@users.noreply.github.com>
2025-11-03 22:30:53 +0600

    Auto commit on Mon 11/03/2025 22:30:53.41

---
+++ Day04/02.if-else.dart
@@ -0,0 +1,19 @@
+// Conditional Statement if, if else, if else if
+
+void main(){
+    var marks = 70;
+    if(marks>=80){
+      print("Result is A+");
+    }
+    else if(marks<80 && marks>=70){
+      print("Result is A");
+    }
+    else if(marks<70 && marks>=60){
+      print("result is A-");
+    
+    }
+    else{
+      print("Result is not A+ or A or A-");
+    }
+  print("Thank you");
+}
